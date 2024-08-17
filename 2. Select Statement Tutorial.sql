48dd0c5ce5174207faf458b0ee5f9778f3b3ddbf
chilyda <103399913+chilyda@users.noreply.github.com>
2024-08-16 19:36:45 -0600

    Select Statement Tutorial

---
+++ 2. Select Statement Tutorial.sql
@@ -0,0 +1,18 @@
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name, 
+last_name,
+age,
+(age + 10) * 10 AS new_age
+FROM parks_and_recreation.employee_demographics;
+
+# Anytype of calculation within MYSQL fooloows the rules of PEMDAS 
+# PEMDAS Order of operations for arithemtic of maths within my SQL. Parenthesis, Exponential, Multiplication, Division, Addition and Subtraction
+
+SELECT DISTINCT gender
+FROM parks_and_recreation.employee_demographics;
+
+SELECT DISTINCT first_name, gender
+FROM parks_and_recreation.employee_demographics;
+
