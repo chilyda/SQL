e07cb9ff32208367f6bc144a3299c681b03afe99
chilyda <103399913+chilyda@users.noreply.github.com>
2024-08-16 22:15:02 -0600

    8. Unions
    Using Unions to combine tables

---
+++ 8. Union.sql
@@ -0,0 +1,26 @@
+#Unions allows us to combine rows of data from separate tables or the same tables
+
+SELECT age, gender
+FROM employee_demographics
+UNION 
+SELECT first_name, last_name
+FROM employee_salary;
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION DISTINCT
+SELECT first_name, last_name
+FROM employee_salary;
+
+SELECT first_name, last_name, 'Old Man' AS label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Male'
+UNION
+SELECT first_name, last_name, 'Old Lady' AS label
+FROM employee_demographics
+WHERE age > 40 AND gender ='Female'
+UNION
+SELECT first_name, last_name, 'Highly Paid Employee' AS label
+FROM employee_salary
+WHERE Salary > 70000
+ORDER BY first_name, last_name;
